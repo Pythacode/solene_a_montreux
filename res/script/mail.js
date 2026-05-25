924032bcf36416514fa1bee4e2476eaf1069ff91
Nathanaëlle <titouan.petit-frulio@laposte.net>
2026-05-25 17:38:36 +0200

    Fix hortographe

--- res/script/mail.js
+++ res/script/mail.js
@@ -2,12 +2,12 @@ const mail = "info@montreuxcomedy.com"
 const subject = "Une artiste à ne pas manquer pour Montreux Comédie"
 const body = `Bonjour,
 
-Je me permets de vous contacter car je pense sincèrement que Solène PETIT--FRULIO aurait toute sa place sur la scène de Montreux Comédie.
+Je me permets de vous contacter car je pense sincèrement que Solène Petit--Frulio aurait toute sa place au festival Montreux Comédie.
 
 Son humour, son univers et son talent méritent d'être partagés avec le public du festival. Je suis convaincu(e) que sa présence serait une vraie valeur ajoutée à la programmation.
 
-Elle est déjà suivie par au moins de nombreux follower et son sketch "CPE en millieux hostile" fait furreur sur les réseaux.
-Vous pourrez la retrouver sur instagram (https://www.instagram.com/solene.petitfrulio/) ainsi que retrouver son sketch sur solene-a-montreux.nathanaelle.org.
+Elle est déjà suivie par au moins de nombreux follower et son sketch "CPE en milieux hostile" fait fureur sur les réseaux.
+Vous pourrez la retrouver sur Instagram (https://www.instagram.com/solene.petitfrulio/) et découvrir son sketch sur solene-a-montreux.nathanaelle.org.
 
 Auriez-vous la possibilité d'étudier sa candidature pour une prochaine édition ?
 
